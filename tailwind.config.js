2bbb7c218e4a41fda14e0f9633b344383a1afa42
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-01-29 15:10:39 +0100

    Add hdr colors
    Tailwind config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": "color(display-p3 0.3786 0 0.4337)",
+        "light-purple": "color(display-p3 0.4928 0 0.4137)",
+        "light-pink": "color(display-p3 0.514 0 0.4262)",
+        "light-amber": "color(display-p3 1 0.8861 0.7734)",
+        "toxic-pink": "color(display-p3 0.8877 0 0.585)",
+        "dark-blue": "color(display-p3 0.0843 0 0.254)",
+      },
+    },
   },
   plugins: [],
-}
+};
